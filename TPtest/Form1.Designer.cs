8cfdf4b36125d10937cfa059392e14cf2529c3f6
Sam <qacboy@gmail.com>
2017-12-14 13:46:52 +0800

    添加项目文件。

---
+++ TPtest/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace TPtest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_open = new System.Windows.Forms.Button();
+            this.btn_save = new System.Windows.Forms.Button();
+            this.btn_readPix = new System.Windows.Forms.Button();
+            this.btn_changePix = new System.Windows.Forms.Button();
+            this.btn_hideText = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btn_showText = new System.Windows.Forms.Button();
+            this.inputText = new System.Windows.Forms.TextBox();
+            this.btn_hintImg = new System.Windows.Forms.Button();
+            this.btn_showImg = new System.Windows.Forms.Button();
+            this.input_key = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_open
+            // 
+            this.btn_open.Location = new System.Drawing.Point(13, 13);
+            this.btn_open.Name = "btn_open";
+            this.btn_open.Size = new System.Drawing.Size(100, 30);
+            this.btn_open.TabIndex = 0;
+            this.btn_open.Text = "打开图像";
+            this.btn_open.UseVisualStyleBackColor = true;
+            this.btn_open.Click += new System.EventHandler(this.btn_open_Click);
+            // 
+            // btn_save
+            // 
+            this.btn_save.Location = new System.Drawing.Point(119, 13);
+            this.btn_save.Name = "btn_save";
+            this.btn_save.Size = new System.Drawing.Size(100, 30);
+            this.btn_save.TabIndex = 1;
+            this.btn_save.Text = "保存图像";
+            this.btn_save.UseVisualStyleBackColor = true;
+            this.btn_save.Click += new System.EventHandler(this.btn_save_Click);
+            // 
+            // btn_readPix
+            // 
+            this.btn_readPix.Location = new System.Drawing.Point(225, 13);
+            this.btn_readPix.Name = "btn_readPix";
+            this.btn_readPix.Size = new System.Drawing.Size(100, 30);
+            this.btn_readPix.TabIndex = 2;
+            this.btn_readPix.Text = "读取像素";
+            this.btn_readPix.UseVisualStyleBackColor = true;
+            this.btn_readPix.Click += new System.EventHandler(this.btn_readPix_Click);
+            // 
+            // btn_changePix
+            // 
+            this.btn_changePix.Location = new System.Drawing.Point(331, 13);
+            this.btn_changePix.Name = "btn_changePix";
+            this.btn_changePix.Size = new System.Drawing.Size(100, 30);
+            this.btn_changePix.TabIndex = 3;
+            this.btn_changePix.Text = "修改像素";
+            this.btn_changePix.UseVisualStyleBackColor = true;
+            this.btn_changePix.Click += new System.EventHandler(this.btn_changePix_Click);
+            // 
+            // btn_hideText
+            // 
+            this.btn_hideText.Location = new System.Drawing.Point(13, 50);
+            this.btn_hideText.Name = "btn_hideText";
+            this.btn_hideText.Size = new System.Drawing.Size(100, 30);
+            this.btn_hideText.TabIndex = 4;
+            this.btn_hideText.Text = "隐藏文本";
+            this.btn_hideText.UseVisualStyleBackColor = true;
+            this.btn_hideText.Click += new System.EventHandler(this.btn_hideText_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(484, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(503, 767);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(13, 234);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(436, 545);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btn_showText
+            // 
+            this.btn_showText.Location = new System.Drawing.Point(119, 50);
+            this.btn_showText.Name = "btn_showText";
+            this.btn_showText.Size = new System.Drawing.Size(100, 30);
+            this.btn_showText.TabIndex = 7;
+            this.btn_showText.Text = "解密文本";
+            this.btn_showText.UseVisualStyleBackColor = true;
+            this.btn_showText.Click += new System.EventHandler(this.btn_showText_Click);
+            // 
+            // inputText
+            // 
+            this.inputText.Location = new System.Drawing.Point(13, 132);
+            this.inputText.Name = "inputText";
+            this.inputText.Size = new System.Drawing.Size(436, 25);
+            this.inputText.TabIndex = 8;
+            this.inputText.Text = "请输入需要隐藏的文本信息";
+            this.inputText.TextChanged += new System.EventHandler(this.inputText_TextChanged);
+            // 
+            // btn_hintImg
+            // 
+            this.btn_hintImg.Location = new System.Drawing.Point(225, 50);
+            this.btn_hintImg.Name = "btn_hintImg";
+            this.btn_hintImg.Size = new System.Drawing.Size(100, 30);
+            this.btn_hintImg.TabIndex = 9;
+            this.btn_hintImg.Text = "隐藏图片";
+            this.btn_hintImg.UseVisualStyleBackColor = true;
+            this.btn_hintImg.Click += new System.EventHandler(this.btn_hintImg_Click);
+            // 
+            // btn_showImg
+            // 
+            this.btn_showImg.Location = new System.Drawing.Point(331, 50);
+            this.btn_showImg.Name = "btn_showImg";
+            this.btn_showImg.Size = new System.Drawing.Size(100, 30);
+            this.btn_showImg.TabIndex = 10;
+            this.btn_showImg.Text = "显示图片";
+            this.btn_showImg.UseVisualStyleBackColor = true;
+            this.btn_showImg.Click += new System.EventHandler(this.btn_showImg_Click);
+            // 
+            // input_key
+            // 
+            this.input_key.Location = new System.Drawing.Point(259, 173);
+            this.input_key.MaxLength = 1;
+            this.input_key.Name = "input_key";
+            this.input_key.Size = new System.Drawing.Size(190, 25);
+            this.input_key.TabIndex = 11;
+            this.input_key.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.input_key.TextChanged += new System.EventHandler(this.input_key_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(217, 15);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "请输入秘钥（一个字符长度）：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(999, 791);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.input_key);
+            this.Controls.Add(this.btn_showImg);
+            this.Controls.Add(this.btn_hintImg);
+            this.Controls.Add(this.inputText);
+            this.Controls.Add(this.btn_showText);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btn_hideText);
+            this.Controls.Add(this.btn_changePix);
+            this.Controls.Add(this.btn_readPix);
+            this.Controls.Add(this.btn_save);
+            this.Controls.Add(this.btn_open);
+            this.Name = "Form1";
+            this.Text = "加解密";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_open;
+        private System.Windows.Forms.Button btn_save;
+        private System.Windows.Forms.Button btn_readPix;
+        private System.Windows.Forms.Button btn_changePix;
+        private System.Windows.Forms.Button btn_hideText;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btn_showText;
+        private System.Windows.Forms.TextBox inputText;
+        private System.Windows.Forms.Button btn_hintImg;
+        private System.Windows.Forms.Button btn_showImg;
+        private System.Windows.Forms.TextBox input_key;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ TPtest/Form1.cs
@@ -0,0 +1,320 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace TPtest
+{
+    public partial class Form1 : Form
+    {
+        Bitmap map;
+        byte[] pix;
+        string sss;
+        static char key;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_open_Click(object sender, EventArgs e)
+        {
+            
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "BMP Files(*.bmp)|*.bmp|JPG File(*.jpg;*.jpeg)|*.jpg;*.jpeg|ALL Files(*.*)|*.*";
+            ofd.CheckFileExists = true;
+            ofd.CheckPathExists = true;
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                map = new Bitmap(ofd.FileName);
+                if (map == null)
+                {
+                    MessageBox.Show("图片加载失败！", "错误");
+                    return;
+                }
+                pictureBox1.Image = map;
+                ofd.Dispose();
+            }
+
+        }
+
+        private void btn_save_Click(object sender, EventArgs e)
+        {
+            if (map == null) return;
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "BMP Files(*.bmp)|*.bmp|JPG File(*.jpg;*.jpeg)|*.jpg;*.jpeg|ALL Files(*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBox1.Image.Save(sfd.FileName, System.Drawing.Imaging.ImageFormat.Bmp);
+                MessageBox.Show("图片保存成功！", "提示");
+                sfd.Dispose();
+            }
+
+        }
+        public static byte[] GetImagePixel(Bitmap img)
+        {
+
+            byte[] result = new byte[img.Width * img.Height * 3];
+            int n = 0;
+            for (int i = 0; i < img.Height; i++)
+            {
+                for (int j = 0; j < img.Width; j++)
+                {
+                    result[n] = img.GetPixel(i, j).R;
+                    result[n + 1] = img.GetPixel(i, j).G;
+                    result[n + 2] = img.GetPixel(i, j).B;
+                    n += 3;
+                }
+            }
+            return result;
+        }
+
+        private void btn_readPix_Click(object sender, EventArgs e)
+        {
+            pix = GetImagePixel(this.map);
+            this.richTextBox1.Clear();
+            foreach (byte pixle in pix)
+            {
+                this.richTextBox1.AppendText(pixle + " ");
+            }
+        }
+
+        private void btn_changePix_Click(object sender, EventArgs e)
+        {
+            if (map == null)
+            {
+                return;
+            }
+            int n = 0;
+            pix = GetImagePixel(this.map);
+            for (int i = 0; i < pix.Length; i++)
+            {
+                if (pix[i] % 2 != 0)
+                {
+                    pix[i] -= 1;
+                }
+            }
+            for (int i = 0; i < map.Width; i++)
+            {
+                for (int j = 0; j < map.Height; j++)
+                {
+                    Color c = Color.FromArgb(pix[n], pix[n + 1], pix[n + 2]);
+                    map.SetPixel(i, j, c);
+                    n += 3;
+                }
+            }
+            this.richTextBox1.Clear();
+            foreach (byte pixle1 in pix)
+            {
+                this.richTextBox1.AppendText(pixle1 + " ");
+            }
+        }
+
+        // 将字符串转成二进制
+        public static string bianma(string bms)
+        {
+          byte[] data = Encoding.Unicode.GetBytes(bms);
+          StringBuilder result = new StringBuilder(data.Length * 8);
+  
+          foreach (byte b in data)
+          {
+              result.Append(Convert.ToString(b, 2).PadLeft(8, '0'));
+          }
+          return result.ToString();
+        }
+
+      // 将二进制转成字符串
+      public static String jiema(String js)
+      {
+            System.Text.RegularExpressions.CaptureCollection cs =
+              System.Text.RegularExpressions.Regex.Match(js, @"([01]{8})+").Groups[1].Captures;
+          byte[] data = new byte[cs.Count];
+          for (int i = 0; i<cs.Count; i++)
+          {
+              data[i] = Convert.ToByte(cs[i].Value, 2);
+          }
+          return Encoding.Unicode.GetString(data, 0, data.Length);
+        }
+      
+
+        /// 异或的加密解密
+        public static String BCutEncrypt(String str)
+        {
+            char emblem = key;//密钥
+            StringBuilder buffer = new StringBuilder();
+            char[] chars = str.ToCharArray();
+            foreach (char ch in chars)
+            {
+                char temp = (char)(ch ^ emblem);
+                buffer.Append(temp);
+            }
+            return buffer.ToString();
+        }
+
+        
+        private void btn_hideText_Click(object sender, EventArgs e)
+        {
+            String s = inputText.Text;
+            String es = BCutEncrypt(s);
+            String bs = bianma(es);
+            this.richTextBox1.Clear();
+            if (map != null)
+            {
+                for (int i = 0; i < bs.Length; i++)
+                {
+                    int pixle = Convert.ToInt32(pix[i]) + Convert.ToInt32(bs[i]) - 48;
+                    pix[i] = Convert.ToByte(pixle);
+                    //pixle[i] = Convert.ToByte(Convert.ToInt32(bs[i])-48);
+                }
+                for (int i = 0, n = 0; i < map.Width; i++)
+                {
+                    for (int j = 0; j < map.Height; j++)
+                    {
+                        Color c = Color.FromArgb(pix[n], pix[n + 1], pix[n + 2]);
+                        map.SetPixel(i, j, c);
+                        n += 3;
+                    }
+                }
+                foreach (byte pixle1 in pix)
+                {
+                    this.richTextBox1.AppendText(pixle1 + " ");
+                }
+                //richTextBox1.AppendText("jiami：" + es + "    2jinzhi:" + bs );
+            }
+            else
+            {
+                this.richTextBox1.AppendText(sss);
+            }
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+        private void btn_showText_Click(object sender, EventArgs e)
+        {
+            string rs = null;
+            this.richTextBox1.Clear();
+            if (map != null)
+            {
+                int p = 0;//判断后面是否有 1 个全为 0 的十六位二进制数，如果有，说明密码文本读取结束
+                for (int i = 0; i < pix.Length; )
+                {
+                    if (p != 16)
+                    {
+                        p = 0;
+                        for (int j = 0; j < 16; j++)
+                        {
+                            int pixle = Convert.ToInt32(pix[i]);
+                            if (pixle % 2 == 1) rs += "1";
+                            else
+                            {
+                                rs += "0";
+                                p++;
+                            }
+                            i++;
+                        }
+                    }
+                    else {
+                        rs=rs.Substring(0, rs.Length - 16);
+                        break;
+                    }
+                }
+                String es = jiema(rs);
+                String bs = BCutEncrypt(es);
+                this.richTextBox1.AppendText(bs);
+                //this.richTextBox1.AppendText("     rs" + rs + "       jiamma:" + es + "     jiemi:" + bs);
+            }
+            else
+            {
+                rs = BCutEncrypt(sss);
+                this.richTextBox1.AppendText("jiemi:"+rs);
+            }
+        }
+
+
+
+
+
+
+
+
+
+        private void inputText_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void btn_hintImg_Click(object sender, EventArgs e)
+        {
+
+            String es = BCutEncrypt(inputText.Text);
+            String bs = bianma(es);
+            sss = bs;
+            this.richTextBox1.Clear();
+            richTextBox1.AppendText("jiami：" + es+"    2jinzhi:"+bs+"     sss"+sss);
+            //if (map == null) return;
+            //String t4 = bianma("5",1);
+            //int kk = 10 ^ 1;
+            //int l = t4.Length;
+            //byte tt = 0;
+            //String tk = Convert.ToString(kk, 2).PadLeft(16, '0');
+            //richTextBox1.AppendText("t4的16位二进制：" + t4 + "      长度：" + l + "      t4转换后："+Convert.ToInt32(t4,2));
+            //richTextBox1.AppendText("t4的16位二进制：" + t4);
+            //String tk1 = Convert.ToString(kk, 2).PadLeft(16, '0');
+            //richTextBox1.AppendText("     16位二进制为：" + tk + "      tk.length:" + tk.Length);
+            //int i = Convert.ToInt32(t4, 2);
+            //int t3 = 2 ^ 13;
+            //char key = 'n';
+            //richTextBox1.AppendText(Convert.ToString(2, 2).PadLeft(16, '0') + "   "+ Convert.ToString(13, 2).PadLeft(16, '0') + "   "+"t3：" + Convert.ToString(t3,2).PadLeft(16, '0'));
+            //string s = BCutEncrypt(t4, key);
+            //string ss = BCutEncrypt(s, key);
+            //richTextBox1.AppendText(t4.PadLeft(16, '0')+"    jiami :" +s+"   jiemi:"+ ss);
+
+            //richTextBox1.AppendText("t4：" + Convert.ToString(i));
+        }
+
+        private void btn_showImg_Click(object sender, EventArgs e)
+        {
+            //if (map == null) return;
+            String es = jiema(sss);
+            String bs = BCutEncrypt(es);
+            this.richTextBox1.AppendText("     sss" + sss+"       jiamma:" +es+"     jiemi:"+bs);
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void input_key_TextChanged(object sender, EventArgs e)
+        {
+            key = Convert.ToChar(input_key.Text);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+        /* private void Form1_Load(object sender, EventArgs e)
+{
+
+}*/
+
+    }
+    
+}
